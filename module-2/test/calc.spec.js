d6a429448a857bd0e68f8afbd7beb81e1ebc82ce
Serhii Yasenev <serhii_yasenev@epam.com>
2018-04-25 18:26:39 +0300

    Module-2 Cover by Unit tests

--- module-2/test/calc.spec.js
+++ module-2/test/calc.spec.js
@@ -4,20 +4,209 @@ const calc = require('../calc');
 const expect = require('chai').expect;
 
 describe.only('calc', () => {
-    /*
-     * calc(3).v // 3
-     * calc(3).add(5).v // 8
-     * calc(3).minus(2).v // 1
-     * calc(4).sqrt().v // 2
-     * calc(3).times(10).v // 30
-     * calc(10).divide(2).v // 5
-     * calc(10).modulo(5).v // 0
-     * calc(5).divide(0) // throw error
-     * calc(-3).sqrt() // throw error
-     * calc(3).add(4)
-     *     .minus(3)
-     *     .times(6).v // 24
-     */
-    // TODO: write test cases to test calculator
+    it('should have proper initial value', () => {
+        // When 
+        const c = calc(3);
+        // Then 
+        expect(c.v).to.be.equal(3);
+    });
 
+    describe('calculations', () => {
+        it('should do several calculations', () => {
+                // Given 
+                const c = calc(3);
+                // When
+                c.add(4).minus(3).times(6);
+                // Then 
+                expect(c.v).to.be.equal(24);
+        });
+    });
+
+    describe('.modulo', () => {
+        it('should have method to modulo', () => {
+            // Given 
+            const c = calc(10);
+            // When
+            c.modulo(5);
+            // Then 
+            expect(c.v).to.be.equal(0);
+        });
+
+        it('modulo should return remainder of the division', () => {
+            // Given 
+            const c = calc(10);
+            // When
+            c.modulo(3);
+            // Then 
+            expect(c.v).to.be.equal(1);
+        });
+
+        it('modulo should return remainder of the division for non-positive initial value', () => {
+            // Given 
+            const c = calc(-10);
+            // When
+            c.modulo(3);
+            // Then 
+            expect(c.v).to.be.equal(-1);
+        });
+
+        it('modulo should return remainder of the division for non-positive divisor', () => {
+            // Given 
+            const c = calc(10);
+            // When
+            c.modulo(-3);
+            // Then 
+            expect(c.v).to.be.equal(1);
+        });
+    });
+
+    describe('.divide', () => {
+        it('should throw an error when devide by 0, because division by 0 is not possible', () => {
+            // Given
+            const c = calc(5);
+            //Then
+            expect(() => c.divide(0)).to.throw();
+        });
+
+        it('should have method to divide value', () => {
+            //Given
+            const c = calc(10);
+            // When 
+            c.divide(5);
+            // Then 
+            expect(c.v).to.be.equal(2); 
+         });
+
+            it('should divide the same value as the current one', () => {
+                // Given
+                const c = calc(5);
+                //When
+                c.divide(5);
+                //Then
+                expect(c.v).to.be.equal(1); 
+            });
+
+            it('should divide for the same, but another sign', () => {
+                // Given
+                const c = calc(5);
+                //When
+                c.divide(-5);
+                //Then
+                expect(c.v).to.be.equal(-1); 
+            });
+        });
+
+    describe('.times', () => {
+        it('should have method to times value', () => {
+            //Given
+            const c = calc(3);
+            // When 
+            c.times(10);
+            // Then 
+            expect(c.v).to.be.equal(30); 
+         });
+
+            it('should time the same value as the current one', () => {
+                // Given
+                const c = calc(5);
+                //When
+                c.times(5);
+                //Then
+                expect(c.v).to.be.equal(25); 
+            });
+
+            it('should time the same value as the current one, but with another sign', () => {
+                // Given
+                const c = calc(5);
+                //When
+                c.times(-5);
+                //Then
+                expect(c.v).to.be.equal(-25); 
+            });
+
+            it('should time to 0', () => {
+                // Given
+                const c = calc(5);
+                //When
+                c.times(0);
+                //Then
+                expect(c.v).to.be.equal(0); 
+            });
+        });
+
+    describe('.minus', () => {
+        it('should have method to minus value', () => {
+            //Given
+            const c = calc(3);
+            // When 
+            c.minus(5);
+            // Then 
+            expect(c.v).to.be.equal(-2); 
+         });
+
+         it('should minus any value to the current one', () => {
+             // Given
+             const c = calc(3);
+             //When
+             c.minus(1);
+             //Then
+             expect(c.v).not.to.be.undefined;
+            });
+
+            it('should minus the same value as the current one', () => {
+                // Given
+                const c = calc(10);
+                //When
+                c.minus(10);
+                //Then
+                expect(c.v).to.be.equal(0); 
+            });
+        });
+
+    describe('.add', () => {
+        it('should have method to add value', () => {
+            //Given
+            const c = calc(3);
+            // When 
+            c.add(5);
+            // Then 
+            expect(c.v).to.be.equal(8);
+         });
+
+         it('should add any value to the current one', () => {
+             // Given
+             const c = calc(3);
+             //When
+             c.add(5);
+             //Then
+             expect(c.v).not.to.be.undefined;
+            });
+        });
+
+    describe('.sqrt', () => {
+        it('should have method to compute square root', () => {
+            // Given
+            const c = calc(3);
+            // When
+            c.sqrt();
+            // Then
+            expect(c.v).not.to.be.undefined;
+        });
+
+        it('should compute the square root on non-negative value', () => {
+            // Given
+            const c = calc(9);
+            // When
+            c.sqrt();
+            // Then
+            expect(c.v).to.be.equal(3);
+        });
+
+        it('should throw an error for negative value', () => {
+            // Given
+            const c = calc(-3);
+            // Then 
+            expect(c.sqrt).to.throw();
+        });
+    });
 });
